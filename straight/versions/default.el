826ac91ac4568dab6ed1ecfdc27695e263725830
Shane Sveller <shanesveller@gmail.com>
2018-12-21 14:22:55 -0600

    Install org-plus-contrib

--- straight/versions/default.el
+++ straight/versions/default.el
@@ -3,6 +3,7 @@
  ("evil" . "82c65dcfe23aff3d764cafc78124d92940c5bd59")
  ("goto-chg" . "c664d8787939dfdf060d0b4cfe1240aeee62ffc8")
  ("melpa" . "378720400eecb7cc94b4aca19c40d1b0440ddd6e")
+ ("org" . "908b55124cb1abdcb3b7b6e3276e0ac22c86d254")
  ("straight.el" . "66fc64bdb7822a199bad77cf56955d70ff6a6f27")
  ("use-package" . "39a8b8812c2c9f6f0b299e6a04e504ef393694ce"))
 :saturn
