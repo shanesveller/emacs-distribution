66b8a275cc09f795d526bf43b28c18dc576a217c
Shane Sveller <shanesveller@gmail.com>
2018-12-21 13:51:54 -0600

    Freeze package versions

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 init.el: distribution.org
 	emacs -Q --batch --eval "(progn (require 'ob-tangle) (dolist (file command-line-args-left) (with-current-buffer (find-file-noselect file) (org-babel-tangle))))" "distribution.org"
 
+.PHONY: freeze-packages
+freeze-packages:
+	emacs -Q -l init.el --batch --eval "(straight-freeze-versions)"
+
 .PHONY: install-packages
 install-packages:
 	emacs -Q -l init.el --batch
---
+++ straight/versions/default.el
@@ -0,0 +1,8 @@
+(("elpa" . "aa311e7205da434a03b3216f58b237483e9b46c1")
+ ("epkgs" . "3e2b3cbada81bdb5e9b3f4bf185c7258f656b021")
+ ("evil" . "82c65dcfe23aff3d764cafc78124d92940c5bd59")
+ ("goto-chg" . "c664d8787939dfdf060d0b4cfe1240aeee62ffc8")
+ ("melpa" . "378720400eecb7cc94b4aca19c40d1b0440ddd6e")
+ ("straight.el" . "66fc64bdb7822a199bad77cf56955d70ff6a6f27")
+ ("use-package" . "39a8b8812c2c9f6f0b299e6a04e504ef393694ce"))
+:saturn
